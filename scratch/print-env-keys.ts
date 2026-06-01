26e8a88ad597697cf46cca2c1318f02d3aee41e9
macbook <macbook@macbook-MacBook-Pro.local>
2026-06-01 15:46:14 +0530

    Untrack local npm cache and scratch directories

--- scratch/print-env-keys.ts
+++
@@ -1,6 +0,0 @@
-console.log('Environment variable keys:', Object.keys(process.env).sort());
-if (process.env.ENCRYPTION_KEY) {
-  console.log('ENCRYPTION_KEY is set! Length:', process.env.ENCRYPTION_KEY.length);
-} else {
-  console.log('ENCRYPTION_KEY is not set.');
-}
